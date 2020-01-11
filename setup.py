75cf8a78411d2cbf794a91553d1cb46a7b914ffb
Marcell Kiss <marcell@kmarcell.com>
2020-01-11 18:49:56 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
     name="connectbox",
     version="0.1",
     author="Marcell Kiss",
-    author_email="info@kmarcell.com",
+    author_email="contact@kmarcell.com",
     description=("Compal Router CH7465LG Python Interface"),
     keywords="compal CH7465LG interface",
     license="GPL",
